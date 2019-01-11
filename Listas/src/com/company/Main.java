d2b3fecf4e6497f9e1b5f3395e84dad1f0efec0b
Ruben Diaz <TheMilubi@gmail.com>
2019-01-11 13:50:00 +0100

    Listas completa

--- Listas/src/com/company/Main.java
+++ Listas/src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int opcion;
         System.out.println("Menu");
         System.out.println("************");
-        System.out.print("Escribe el numero del ejercicio [1-20]: ");
+        System.out.print("Escribe el numero del ejercicio [1-18]: ");
         opcion = sc.nextInt();
         switch (opcion) {
             case 1:{
@@ -161,10 +161,63 @@ public class Main {
                 Integer[] c = insertaArrayEnArrayPro(a,b,2);
                 System.out.println(Arrays.toString(c));
             }break;
+            case 12:{
+                List<Integer> g = sorteoBonoloto();
+                System.out.println(g);
+            }break;
             case 13:{
                 Integer[] a = {1,2,3,2,1,1,4,3,4,5};
                 Integer[] r = eliminaRepetidos(a);
                 System.out.println(Arrays.toString(r));
+            }break;
+            case 14:{
+                List<Integer> la = new LinkedList<>();
+                List<Integer> lb = new LinkedList<>();
+                for(int i = 0; i < 6; i++){
+                    la.add(i);
+                    lb.add(i+4);
+                }
+                List<Integer> r = unionListas(la,lb);
+                System.out.println(r);
+            }break;
+            case 15:{
+                List<Integer> la = new LinkedList<>();
+                List<Integer> lb = new LinkedList<>();
+                for(int i = 0; i < 6; i++){
+                    la.add(i);
+                    lb.add(i+3);
+                }
+                List<Integer> r = interseccionListas(la,lb);
+                System.out.println(r);
+            }break;
+            case 16:{
+                List<Integer> la = new LinkedList<>();
+                for(int i = 0; i < 10; i++){
+                    la.add(i);
+                }
+                desordenaLista(la);
+                System.out.println(la);
+            }break;
+            case 17:{
+                Random r = new Random();
+                List<Integer> la = new LinkedList<>();
+                for(int i = 0; i < 10; i++){
+                    la.add(i);
+                    la.add(r.nextInt(10));
+                }
+                desordenaLista(la);
+                System.out.println(la);
+                System.out.println(modaLista(la));
+            }break;
+            case 18:{
+                Random r = new Random();
+                List<Double> la = new LinkedList<>();
+                for(int i = 0; i < 7; i++){
+                    la.add(r.nextDouble()+r.nextInt(10));
+                }
+                double punt = puntuacionesTrampolin(la);
+                System.out.println(la);
+                System.out.println(punt);
             }break;
         }
 
@@ -283,9 +336,7 @@ public class Main {
     //- Repetimos hasta que nuestra lista se quede vacía y todos los elementos hayan pasado a la lista resultado en orden.
     public static void ordenaListaInsercion(List<Integer> list){
         List<Integer> ordenado = new LinkedList<>();
-        ordenado.add(list.get(0));
-        list.remove(0);
-        for(int j = 0; j < list.size(); j ++){
+        while(!list.isEmpty()){
             int actual = list.get(0);
             int i = 0;
             while(i<ordenado.size()&&actual>ordenado.get(i)){
@@ -293,7 +344,6 @@ public class Main {
             }
             ordenado.add(i,actual);
             list.remove(0);
-            j--;
         }
         list.clear();
         list.addAll(ordenado);
@@ -351,9 +401,20 @@ public class Main {
     // Para ello, meteremos en una lista los 49 números, elegiremos una posición al azar, guardaremos el
     // número en otra lista y lo eliminaremos de la primera (para que no pueda volver a salir). Al terminar
     // de sortear los 6 números, devolveremos una lista que contenga el resultado.
-    /*public static List<Integer> sorteoBonoloto(){
-
-    }*/
+    public static List<Integer> sorteoBonoloto(){
+        List<Integer> list = new LinkedList<>();
+        List<Integer> ganador = new LinkedList<>();
+        Random r = new Random();
+        for(int i = 0; i < 50; i++){
+            list.add(i);
+        }
+        for(int i = 0; i < 6; i++){
+            int pos = r.nextInt(list.size());
+            ganador.add(list.get(pos));
+            list.remove(pos);
+        }
+        return ganador;
+    }
     //13. Escribe la función eliminaRepetidos a la que le pasamos un array (en el que puede que aparezcan
     // elementos repetidos) y nos devuelve otro array en el que no hay repetidos. Podemos usar listas para
     // que sea más sencillo.
@@ -361,5 +422,87 @@ public class Main {
         List<Integer> lista = Arrays.asList(array);
         eliminaRepetidos(lista);
         return lista.toArray(Integer[]::new);
+    }
+    //14. Escribe la función unionListas a la que le pasas dos listas (que representan conjuntos) y que te
+    // devuelve otra lista que representará la unión de ambos conjuntos. Esto es, los elementos que estén
+    // o en la primera lista o en la segunda (si están en las dos, sólo hay que meterlos una vez).
+    public static List<Integer> unionListas(List<Integer> la, List<Integer> lb){
+        List<Integer> r = new LinkedList<>();
+        r.addAll(la);
+        for (int i = 0; i < lb.size(); i++){
+            if(!r.contains(lb.get(i))){
+                r.add(lb.get(i));
+            }
+        }
+        return r;
+    }
+    //15. Escribe la función interseccionListas a la que le pasas dos listas (que representan conjuntos) y
+    // que te devuelve otra lista que representará la intersección de ambos conjuntos. Esto es, los elementos
+    // que estén tanto en la primera lista como en la segunda.
+    public static List<Integer> interseccionListas(List<Integer> la, List<Integer> lb){
+        List<Integer> r = new LinkedList<>();
+        for (int i = 0; i < lb.size(); i++){
+            if(la.contains(lb.get(i))){
+                r.add(lb.get(i));
+            }
+        }
+        return r;
+    }
+    //16. Escribe la función desordenaLista a la que le pasas una lista y te la desordena. Para ello, iremos
+    // cogiendo al azar elementos de la lista y poniéndolos en otra lista (quitándolos de la primera). La
+    // segunda lista, contendrá los elementos al azar.
+    public static void desordenaLista(List<Integer> list){
+        Random r = new Random();
+        List<Integer> desordenada = new LinkedList<>();
+        while(!list.isEmpty()){
+            int posicion = r.nextInt(list.size());
+            desordenada.add(list.get(posicion));
+            list.remove(posicion);
+        }
+        list.clear();
+        list.addAll(desordenada);
+    }
+    //17. Escribe la función modaLista que nos devuelva la moda (el valor que más veces se repite) de una
+    // lista de enteros.
+    public static int modaLista(List<Integer> list){
+        int moda = 0, contadorModa = 0,maxCont = 0;
+        List<Integer> copia = new LinkedList<>();
+        copia.addAll(list);
+        for(int i = 0; i < list.size();i++){
+            for(int j = i+1; j < list.size();j++){
+                if(copia.get(i)==copia.get(j)){
+                    contadorModa++;
+                }
+            }
+            if(contadorModa>maxCont){
+                moda = copia.get(i);
+                maxCont = contadorModa;
+            }
+            contadorModa= 0;
+        }
+        return moda;
+    }
+    //18. Escribe la función puntuacionesTrampolin. La función recibirá una lista con siete números reales
+    // que se corresponderán a las notas obtenidas por un saltador de trampolín de 3 metros. En este
+    // deporte, para calcular la nota final, se eliminan las dos notas más altas y las dos más bajas,
+    // sumándose las tres que quedan. Por tanto, nuestra función eliminará los dos valores más altos
+    // y los dos más bajos del la lista y, además, devolverá un real que será la suma de los tres
+    // valores que quedan.
+    public static double puntuacionesTrampolin(List<Double> ld){
+        double suma = 0;
+        if(ld.size()!=7){
+            suma = -1;
+            System.out.println("La lista debe tener 7 puntuaciones");
+        }else{
+            ld.sort(Double::compareTo);
+            ld.remove(0);
+            ld.remove(0);
+            ld.remove(ld.size()-1);
+            ld.remove(ld.size()-1);
+            for (double e:ld) {
+                suma+=e;
+            }
+        }
+        return suma;
     }
 }
